81f3b4e454eb4b7c6b7c4eba1f7eb25191e5b59f
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2024-04-08 21:58:22 -0400

    archive page halfway done

--- archive.php
+++ archive.php
@@ -1,40 +1,138 @@
 <?php get_header(); ?>
 
-<div class="main">
-  <div class="container">
-    <div class="content">
+<main>
+  <section class="container">
+    
+    
+  <?php
 
-      <?php if ( have_posts() ) the_post(); ?>
+  
+// Get the current product category ID
+$category_id = get_queried_object_id();
 
-      <h1>
-        <?php if ( is_day() ) : ?>
-          Daily Archives: <?php the_date(); ?>
-        <?php elseif ( is_month() ) : ?>
-          Monthly Archives: <?php the_date('F Y'); ?>
-        <?php elseif ( is_year() ) : ?>
-          Yearly Archives: <?php the_date('Y'); ?>
-        <?php else : ?>
-          Blog Archives
-        <?php endif; ?>
-      </h1>
+$selected_category_id = $category_name; //
+$category = get_term($selected_category_id);
+print_r($category);
+$category_name = $category->name;
 
-      <?php
-    	/* Since we called the_post() above, we need to
-    	 * rewind the loop back to the beginning that way
-    	 * we can run the loop properly, in full.
-    	 */
-    	rewind_posts();
 
-    	/* Run the loop for the archives page to output the posts.
-    	 * If you want to overload this in a child theme then include a file
-    	 * called loop-archives.php and that will be used instead.
-    	 */
-      get_template_part( 'loop', 'archive' );
-      ?>
+// Retrieve custom field values
+$background_color = get_term_meta($category_id, 'background_colour_cat', true);
+$highlight_color = get_term_meta($category_id, 'highlight_colour_cat', true);
 
-    </div><!--/content-->
+$args = array(
+    'posts_per_page' => 10, // Display 10 entries per page
+    'paged' => get_query_var('paged') ? get_query_var('paged') : 1, // Pagination
+    'post_type' => 'product',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'site-category',
+            'field' => 'term_id',
+            'terms' => $category_id
+        )
+    ),
+    'meta_query' => array(
+        array(
+            'key' => '_stock_status',
+            'value' => 'instock'
+        )
+    )
+);
 
-  </div> <!-- /.container -->
-</div> <!-- /.main -->
+$products = new WP_Query($args);
+
+if ($products->have_posts()) :
+    // Output category background color
+    echo "<div style='background-color: $background_color;'>";
+
+    // Output category highlight color
+    echo "<div style='background-color: $highlight_color;'>";
+    echo "<h2>$category_name</h2>";
+  
+    // Output container for products
+    echo "<ul class='product-container'>";
+    
+
+    while ($products->have_posts()) : $products->the_post();
+        global $product;
+        $product_id = $product->get_id();
+        $product_link = get_permalink($product_id);
+        $product_title = get_the_title($product_id);
+        $product_image = get_the_post_thumbnail($product_id, 'thumbnail');
+        $add_to_cart_url = $product->add_to_cart_url();
+        $product_gallery_ids = $product->get_gallery_image_ids();
+        ?>
+      
+
+    <li class="product-item">
+        <a href="<?php echo $product_link; ?>">
+
+        <?php if ($product_image): ?>
+        <div class="product-image">
+            <?php echo $product_image; ?>
+        </div>
+      <?php else: ?>
+        <div class="product-image">
+            <?php
+            global $product;
+
+            if ( $product ) {
+                $post_thumbnail_id = $product->get_image_id();
+            }
+
+            if ( $post_thumbnail_id ) {
+                $html = wc_get_gallery_image_html( $post_thumbnail_id, true );
+            } else {
+                $html  = '<div class="woocommerce-product-gallery__image--placeholder">';
+                $html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+                $html .= '</div>';
+            }
+
+            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+            ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($product_gallery_ids)) {
+                    echo "<div class='product-carousel'>";
+                    foreach ($product_gallery_ids as $gallery_id) {
+                        echo wp_get_attachment_image($gallery_id, 'thumbnail');
+                    }
+                    echo "</div>";
+           }?>
+          </a>
+          <section class="product_info">
+          <h4><a href="<?php echo $product_link; ?>"><?php echo $product_title; ?></a></h4>
+      
+          <a href="<?php echo $add_to_cart_url; ?>" class="add-to-cart-btn">Add to Cart</a>
+
+          </section>
+
+      </li>
+
+    <?php endwhile;
+
+    // Pagination
+    echo "<div class='pagination'>";
+    echo paginate_links(array(
+        'total' => $products->max_num_pages
+    ));
+    echo "</div>";
+
+    // Close containers
+    echo "</div>"; // .product-container
+    echo "</div>"; // .highlight-color
+    echo "</div>"; // .background-color
+
+    // Reset Post Data
+    wp_reset_postdata();
+else :
+    // No posts found
+    echo '<p>No products found</p>';
+endif;
+?>
+
+  </section>
+  
+</main>
 
 <?php get_footer(); ?>
